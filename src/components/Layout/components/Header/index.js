c25f1b1a473a9f9c99680502bfa9ea1272ad2365
TranPhucTien <tranphuctien2003nd@gmail.com>
2022-05-07 19:11:45 +0700

    Dựng khung DefaultLayout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss'
+
+const cx = classNames.bind(styles) // Dung de viet dau `-`. Vd: post-item. Neu khong dung: styles['post-item']. Neu dung: cx('post-item')
+
 function Header() {
     return ( 
-        <h2>Header</h2>
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* Logo */}
+                {/* Search */}
+            </div>
+        </header>
     );
 }
 
