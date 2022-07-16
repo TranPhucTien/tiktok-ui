462d77919d2ff106c2917d8baf0e39c40069384a
TranPhucTien <tranphuctien2003nd@gmail.com>
2022-07-16 19:46:32 +0700

    fix some error

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -5,17 +5,17 @@ import images from '~/assets/images';
 import styles from './Image.module.scss';
 
 const Image = forwardRef(({ src, alt, className, fallback: customFallback = images.noImage, ...props }, ref) => {
-    const [falseback, setFalseback] = useState('');
+    const [fallback, setFallback] = useState('');
 
     const handleError = () => {
-        setFalseback(customFallback);
+        setFallback(customFallback);
     };
 
     return (
         <img
             className={classNames(styles.wrapper, className)}
             ref={ref}
-            src={falseback || src}
+            src={fallback || src}
             alt={alt}
             {...props}
             onError={handleError}
